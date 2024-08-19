68f1f08460fd709f1e0faee740230417bbb67c8c
pedro21ribeiro <pedr21ribeiro7@gmail.com>
2024-08-19 16:34:55 -0300

    Atualização de Remap Aparentemente o use <leader> quando é a barra de espaço não é totalmente estável Por isso modifiquei o uso de <leader> no remap de abrir a arvore para usar o valor puro de <Space>

--- lua/peterpedro/remaps.lua
+++ lua/peterpedro/remaps.lua
@@ -9,4 +9,6 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
+
+
+vim.keymap.set("n", "<Space>t", ":NvimTreeToggle<CR>")
